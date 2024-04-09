d832f4c652365e0c8ca961650bcbf008a178c937
Robert <dozarobert98@gmail.com>
2024-04-09 18:18:29 +0200

    Implement set_to_string function

--- include/set_ops.hpp
+++ include/set_ops.hpp
@@ -3,11 +3,15 @@
 
 #include <unordered_set>
 
+// TODO: remove var names
+
 std::unordered_set<int> set_union(const std::unordered_set<int> &set1, const std::unordered_set<int> &set2);
 
 std::unordered_set<int> set_intersection(const std::unordered_set<int> &set1, const std::unordered_set<int> &set2);
 
 std::unordered_set<int> set_difference(const std::unordered_set<int> &set1, const std::unordered_set<int> &set2);
+
+std::string set_to_string(const std::unordered_set<int>&);
 
 std::ostream& operator << (std::ostream &out, const std::unordered_set<int> &set);
 
--- src/set_ops.cpp
+++ src/set_ops.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 #include "set_ops.hpp"
 
@@ -32,18 +33,31 @@ std::unordered_set<int> set_difference(const std::unordered_set<int> &set1, cons
 	return result;
 }
 
-std::ostream& operator << (std::ostream &out, const std::unordered_set<int> &set) {
+std::string set_to_string(const std::unordered_set<int> &set) {
 	if (set.empty()) {
-		out << "{}";
-		return out;
+		return "{}";
 	}
 	
-	out << "{";
-
+	std::stringstream buffer;
+	
+	buffer << "{";
+	
+	unsigned i = 1;
 	for (const int &element : set) {
-		out << element << ", ";
+		buffer << element;
+		if (i != set.size()) {
+			buffer << ", ";
+		}
+		i++;
 	}
-	out << "\b\b}";
+	
+	buffer << "}";
+	
+	return buffer.str();
+}
+
+std::ostream& operator << (std::ostream &out, const std::unordered_set<int> &set) {
+	out << set_to_string(set);
 	
 	return out;
 }
