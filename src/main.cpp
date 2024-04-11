1b7a98b965cf26e2fc88b26963250835115bd18f
Robert <dozarobert98@gmail.com>
2024-04-11 15:29:01 +0200

    Modify program to accept directory path as a command line argument

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,19 @@
 
 #include "test.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		std::cerr << "Usage: " << argv[0] << " <directory_path>\n";
+		return 1;
+	}
+
+	std::string directory_path = argv[1];
+	
 	try {
-		test_graph("assets/blue_graph");
+		test_graph(directory_path);
 	} catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
 	}
-		
+
 	return 0;
 }
